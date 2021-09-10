01a0adfe2d05b913cb4cee7f29eb10449c76a800
Othman2001 <othman-dev@outlook.com>
2021-09-10 22:53:26 +0200

    fixing issues

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,7 +2,6 @@ import React from 'react'
 import '../css/home.css'
 import { Button } from '@material-ui/core';
 import { motion } from "framer-motion";
-import coding from "../images/c.jpg";
 import Robot from "./Robot";
 function HomePage(){
     return(
@@ -12,7 +11,6 @@ function HomePage(){
            <div className="home__main">
                <main className="home__grid">
                    <div className="home_part1">
-                      
                    <motion.h1
                    animate = {{  scale: [1, 2, 2, 1, 1],
                     rotate: [0, 0, 270, 270, 0],
@@ -23,7 +21,6 @@ function HomePage(){
                    <br />
                    Front   <span className= "span">  end developer </span> 
                     </motion.h1>
-                
                    <p className = "home__main__text">
                    Front-End-Developer / ReactJS </p>
                    <Button variant="outlined" className="home__button">
@@ -34,7 +31,6 @@ function HomePage(){
                    <div className="home_part2">
                 <Robot />
                    </div>
-       
                </main>
            </div>
         </div>
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -4,7 +4,6 @@ import PersonIcon from '@material-ui/icons/Person';
 import "../css/sidebar.css"
 
 import VisibilityIcon from '@material-ui/icons/Visibility';
-import EmailIcon from '@material-ui/icons/Email';
 import FacebookIcon from '@material-ui/icons/Facebook';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import GitHubIcon from '@material-ui/icons/GitHub';
